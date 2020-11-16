ed8a1965f4a0b08f230491a2179dd94f5e31db39
Veres Levi <vereslevi16@gmail.com>
2020-11-16 18:35:04 +0100

    Achievement class kesz

--- 3.Féléves feladat/Models/Achievement.cs
+++ 3.Féléves feladat/Models/Achievement.cs
@@ -4,7 +4,15 @@ using System.Text;
 
 namespace Models
 {
+    public enum AchiLevel
+    {
+        bronze , silver , gold    
+    }
     public class Achievement
     {
+        public string AchiId { get; set; }
+        public string Name { get; set; }
+        public AchiLevel achiLevel { get; set; }
+
     }
 }
