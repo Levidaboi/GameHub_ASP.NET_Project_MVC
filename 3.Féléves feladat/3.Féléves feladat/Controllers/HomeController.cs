879c3bbe69d267fb4a48d6df4e8a2922ec9f4620
Veres Levi <vereslevi16@gmail.com>
2020-11-17 18:01:05 +0100

    adatbazis kiprobalva --> MUKODIK

--- 3.Féléves feladat/3.Féléves feladat/Controllers/HomeController.cs
+++ 3.Féléves feladat/3.Féléves feladat/Controllers/HomeController.cs
@@ -4,13 +4,48 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
+using Logic;
+using Models;
+
 namespace _3.Féléves_feladat.Controllers
 {
     public class HomeController : Controller
     {
+        UserLogic userLogic;
+        GameLogic gameLogic;
+        AchiLogic achiLogic;
+
+        public HomeController(UserLogic userLogic, GameLogic gameLogic, AchiLogic achiLogic)
+        {
+            this.userLogic = userLogic;
+            this.gameLogic = gameLogic;
+            this.achiLogic = achiLogic;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpGet]
+        public IActionResult AddUser()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddUser(User u)
+        {
+            u.Id = Guid.NewGuid().ToString();
+            userLogic.AddNewUser(u);
+            return RedirectToAction(nameof(List));
+        }
+
+        public IActionResult List()
+        {
+            return View(userLogic.GetUsers());
+        }
+
     }
 }
