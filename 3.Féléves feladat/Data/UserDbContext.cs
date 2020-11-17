0793bb09db31e44641d6d656e53b6f13d6ccc60c
Veres Levi <vereslevi16@gmail.com>
2020-11-17 15:18:05 +0100

    data reteg kesz

---
+++ 3.Féléves feladat/Data/UserDbContext.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+
+namespace Data
+{
+    public class PlayersDbContext : DbContext
+    {
+       
+        public PlayersDbContext(DbContextOptions<PlayersDbContext> opt) : base(opt)
+        {
+
+        }
+
+        
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.
+                    UseLazyLoadingProxies().
+                    UseSqlServer(@"data source=(LocalDB)\MSSQLLocalDB;attachdbfilename=|DataDirectory|\UserDb.mdf;integrated security=True;MultipleActiveResultSets=True");
+            }
+        }
+
+        public PlayersDbContext()
+        {
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelbuilder)
+        {
+            base.OnModelCreating(modelbuilder);
+
+            modelbuilder.Entity<Achievement>(entity =>
+            {
+                entity
+                .HasOne(Achievement => Achievement.Game)
+                .WithMany(Game => Game.Achievements)
+                .HasForeignKey(Achievement =>Achievement.GameId);
+            });
+
+
+
+            modelbuilder.Entity<Game>(entity =>
+            {
+                entity
+                .HasOne(Game => Game.User)
+                .WithMany(User => User.GameLibrary)
+                .HasForeignKey(Game => Game.UserId);
+            });
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Achievement> Achievements { get; set; }
+
+    }
+}
