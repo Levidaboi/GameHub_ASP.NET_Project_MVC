6f338836c399edf91087ebb41bbb16abf95ea60c
Veres Levi <vereslevi16@gmail.com>
2020-11-17 17:08:59 +0100

    userrepo kesz

---
+++ 3.Féléves feladat/ClassLibrary1/Class1.cs
@@ -0,0 +1,46 @@
+﻿using Repository;
+using System;
+using Models;
+using System.Linq;
+
+
+
+namespace ClassLibrary1
+{
+    public class UserLogic
+    {
+        UserRepo userrepo;
+
+        public UserLogic(UserRepo userrepo)
+        {
+            this.userrepo = userrepo;
+        }
+
+        public void AddNewUser(User u)
+        {
+            userrepo.Add(u);
+        }
+
+        public void DeleteUser(string uid)
+        {
+            userrepo.Delete(uid);
+        }
+
+        public IQueryable<User> GetUsers()
+        {
+           return userrepo.AllItem();
+        }
+
+        public User GetUser(string uid)
+        {
+            return userrepo.Read(uid);
+        }
+
+        public void UpdateUser(string oldid, User newitem)
+        {
+            userrepo.Update(oldid, newitem);
+        }
+
+
+    }
+}
